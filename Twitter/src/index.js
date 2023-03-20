6c34eaff5e1bf85ac6c3248683365a3d6d156376
girikgarg8 <girikgarg8@gmail.com>
2023-03-20 11:00:42 +0530

    added controllers and routes for tweets

--- Twitter/src/index.js
+++ Twitter/src/index.js
@@ -1,15 +1,15 @@
+import bodyParser from "body-parser";
 import  express from "express";
 import connect from "./config/database.js"
 import apiRoutes from './routes/index.js'
+import service from "./services/tweet-service.js"
 const app = express();
 
-app.use('/api',apiRoutes);
-
-import service from "./services/tweet-service.js"
 app.listen(3000, async () => {
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended:true}))
+    app.use('/api', apiRoutes);
     console.log('Service started')
     await connect();
     console.log('Mongodb connected')
-    let ser = new service();
-    await ser.create({ content: 'Does the new #CodE #Work ?' })
 }) 
