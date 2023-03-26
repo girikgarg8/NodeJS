3ee769c36d0e9fbca5ef00517a282ef28b08deac
girikgarg8 <girikgarg8@gmail.com>
2023-03-26 19:43:22 +0530

    Added get tweet service and routes

--- Twitter/src/routes/v1/index.js
+++ Twitter/src/routes/v1/index.js
@@ -1,6 +1,6 @@
 import express from 'express'
 
-import { createTweet} from '../../controllers/tweet-controller.js'
+import { createTweet,getTweet} from '../../controllers/tweet-controller.js'
 import { toggleLike } from '../../controllers/like-controller.js';
 import {createComment} from '../../controllers/comment-controller.js'
 const router=express.Router();
@@ -8,6 +8,6 @@ const router=express.Router();
 router.post('/tweets',createTweet);
 router.post('/likes/toggle',toggleLike);
 router.post('/comment',createComment);
-
+router.get('/tweets/:id',getTweet);
 export default router;
 
