a95ceef4dc4fc79acbf53d6f60d321ff08e60909
girikgarg8 <girikgarg8@gmail.com>
2023-04-04 19:55:32 +0530

    Working on sockets

--- Chat_App/index.js
+++ Chat_App/index.js
@@ -1,14 +1,14 @@
-const express=require('express');
-const http=require('http');
-const socketio=require('socket.io');
+const express = require('express');
+const http = require('http');
+const socketio = require('socket.io');
+const connect=require('./config/database_config.js')
+const app = express();
 
-const app=express();
+const server = http.createServer(app);
+const io = socketio(server)
 
-const server=http.createServer(app);
-const io=socketio(server)
-
-io.on('connection',(socket)=>{
-    console.log('A user connected ',socket.id)
+io.on('connection', (socket) => {
+    console.log('A user connected ', socket.id)
 
     // socket.on('from_client',()=>{
     //     console.log("event coming from client")
@@ -20,14 +20,28 @@ io.on('connection',(socket)=>{
     //     socket.emit('from_server_2', { data: 'Hello from the server-2!' })
     // }, 1000)
 
-    socket.on('msg_send',(data)=>{
-        console.log(data);
-        socket.broadcast.emit('msg_rcvd',data);
+    socket.on('join_room',(roomid)=>{
+        socket.join(roomid)
     })
+    socket.on('msg_send', (data) => {
+        console.log(data);
+        io.emit('msg_rcvd',data);
+    })
+
+
 });
 
-app.use('/',express.static(__dirname+'/public')) //this tellsthe path of all static files in the folder structure
+app.set('view engine','ejs');
+app.use('/', express.static(__dirname + '/public')) //this tellsthe path of all static files in the folder structure
 
-server.listen(3000,()=>{
+app.get('/chatid/:roomid',async (req,res)=>{
+    res.render('index',{
+        name:'Girik ',
+        id:req.params.roomid
+    })
+})
+server.listen(3000, async () => {
     console.log('Server started')
+    await connect();
+    console.log('MongoDB connected')
 })
--- Chat_App/public/script.js
+++ Chat_App/public/script.js
@@ -1,28 +1,21 @@
 var socket = io();
 
-let btn=document.getElementById('btn');
-let inputMsg=document.getElementById('newMsg');
-let msgList=document.getElementById('msgList');
+let btn = document.getElementById('btn');
+let inputMsg = document.getElementById('newmsg');
+let msgList = document.getElementById('msglist');
 
-// btn.addEventListener('click', function () {
-//     socket.emit('from_client');
-// });
 
-btn.addEventListener('click',function (){
-    socket.emit('msg_send',{
-        msg:inputMsg.value
-    })
-})
+btn.addEventListener('click', function exec() {
+    socket.emit('msg_send', {
+        msg: inputMsg.value
+    });
+});
 
-socket.on('msg_rcvd',(data)=>{
-    let limsg=document.createElement('li');
-    limsg.innerText=data.msg;
-    msgList.appendChild(limsg)
-})
-
-socket.on('from_server',()=>{
-    // console.log("Collected a new event from server")
-    const div=document.createElement('div');
-    div.innerText='New event from server';
-    document.body.appendChild(div);
+socket.on('msg_rcvd', (data) => {
+    let limsg = document.createElement('li');
+    limsg.innerText = data.msg;
+    console.log(document);
+    console.log(limsg);
+    console.log(msgList);
+    msgList.appendChild(limsg);
 })
